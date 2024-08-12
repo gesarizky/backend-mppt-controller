05579005ea0d50eba2136302ae47cb23c1bfff67
gesarizky <gesarizkynugraha@gmail.com>
2024-08-12 11:21:18 +0700

    update credential

--- services_install.js
+++ services_install.js
@@ -4,7 +4,7 @@ const svc = new Service({
   name: "Mppt control",
   description: "This is program node.js for controlling mppt.",
   script:
-    "C:\\Users\\H P\\Desktop\\talis30\\talis30client\\talis30-client-mppt\\index.js", //path program nodejs change \ to \\
+    "C:\\Users\\H P\\Desktop\\project\\backend-mppt-controller\\index.js", //path program nodejs change \ to \\
 });
 
 svc.on("install", () => {
--- services_uninstall.js
+++ services_uninstall.js
@@ -4,7 +4,7 @@ const svc = new Service({
   name: "Talis30 Mppt control",
   description: "This is program node.js for talis30 controlling mppt.",
   script:
-    "C:\\Users\\H P\\Desktop\\talis30\\talis30client\\talis30-client-mppt\\index.js", //path program nodejs change \ to \\
+    "C:\\Users\\Desktop\\project\\backend-mppt-controller\\index.js", //path program nodejs change \ to \\
 });
 
 svc.on("uninstall", () => {
